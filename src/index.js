defe62431688b523ea07baffa4c3839869ef0a69
MiaFate <miapurplefroot@gmail.com>
2022-04-29 16:22:12 -0300

    chakra

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 //import ReactDOM from 'react-dom';
-import {createRoot} from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from '@chakra-ui/react'
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
-<App tab="home"/>
+  <ChakraProvider tab="home">
+    <App/>
+  </ChakraProvider>
 );
 
 /* ReactDOM.render(
