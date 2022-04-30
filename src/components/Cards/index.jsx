53370424c75827a0070e4ba94fa24d175127a507
MiaFate <miapurplefroot@gmail.com>
2022-04-29 22:42:46 -0300

    margin left and right to grid

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -4,7 +4,7 @@ import { Grid } from '@chakra-ui/react'
 const Cards = ({ data: gifData }) => {
 
   return (
-    <Grid templateColumns='repeat(4, 1fr)' gap={3} id="cards" >
+    <Grid templateColumns='repeat(4, 1fr)' gap={3} id="cards" ml={'2rem'} mr={'2rem'}>
       {gifData ? gifData.map(gif => (
         <Card gif={gif} key={gif.id} />
       )) : ""}
