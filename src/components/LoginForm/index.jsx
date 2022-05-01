bb7fa1351035df81ef852578d882012dc7f6656a
MiaFate <miapurplefroot@gmail.com>
2022-05-01 17:01:31 -0300

    users paso de json a js y ahora es un import, se agrego el redirect al home cuando no esta logueado y al app cuando esta logueado y se agrego el dark mode

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -2,36 +2,49 @@ import React from 'react';
 import { useNavigate } from "react-router-dom";
 import { VStack, Center, Button, Input } from "@chakra-ui/react";
 import { useForm } from "react-hook-form";
-const users = require("../../bd/users.json");
+import { useState } from "react";
+import users from "../../bd/users";
 
 const LoginForm = () => {
+    const [datos, setDatos] = useState({});
     const { register, handleSubmit, formState: { errors } } = useForm({defaultValues: {username:"", password:""}});
     const navigate = useNavigate();
 
-    const onSubmit = (data) => {        
-        checkUser(data?.username, data?.password);
+    const onSubmit = (data) => {
+        setDatos(data);
+        const found = checkUser(data.username, data.password);
+        redirect(found);
     };
-
+    console.log(datos);
     const checkUser = (usuario, contraseña) => {
         const userFound =  users.find(user => user.username === usuario && user.password === contraseña);
-        if (userFound) {
-            navigate("/home" )
-        } else {
+        return Boolean(userFound);
+    };
+    const redirect = (found) => {
+        if (found) {
+            localStorage.setItem("logged", true);
+            navigate("/home");
+        }else{
             alert("Usuario o contraseña incorrectos");
         }
-    };    
+    };
+
+
     
     return (
         <Center mt={8} spacing="3px" margin="auto"  >
             <form onSubmit={handleSubmit(onSubmit)}>
                 <VStack textAlign={"center"} align="center" justify={"center"} mt={8} spacing="3px">
+                    
                     <Input type="text" placeholder="Usuario" {...register("username", { required: "Ingrese su nombre de usuario" })} />
                     {errors.username && <p>{errors.username.message}</p>}
                     <Input type="password" placeholder="Contraseña" {...register("password", { required: "ingrese su contraseña" })} />
                     {errors.password && <p>{errors.password.message}</p>}
                     <Button
                         borderRadius="md"
-                        bg="cyan.600"
+                        //bg="cyan.600"
+                        //bg={"brand"}
+                        bg="pink.400"
                         _hover={{ bg: "cyan.200" }}
                         variant="ghost"
                         type="submit"
